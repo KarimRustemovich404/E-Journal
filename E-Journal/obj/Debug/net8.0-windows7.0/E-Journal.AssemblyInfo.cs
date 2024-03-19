3487174fa5586a6e08a69654da1fd94b546a6f87
EgorShabardin <eg.shabardin@gmail.com>
2024-03-19 20:08:56 +0300

    /

--- E-Journal/obj/Debug/net8.0-windows7.0/E-Journal.AssemblyInfo.cs
+++ E-Journal/obj/Debug/net8.0-windows7.0/E-Journal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60598f4878564f393f0eb08045a9e0410eb2b7ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c609ada96be2720992ed961fee71e932e29b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
